721f9f7ddc6798ec3a208c085af2aa33c48b9eba
ahmad-tanha <ahmadtanha1374@gmail.com>
2022-04-27 17:34:55 +0430

    added subscript code

---
+++ Python Exercises/Subscript.py
@@ -0,0 +1,10 @@
+# function to convert to subscript
+def get_sub(x):
+	normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
+	sub_s = "ₐ₈CDₑբGₕᵢⱼₖₗₘₙₒₚQᵣₛₜᵤᵥwₓᵧZₐ♭꜀ᑯₑբ₉ₕᵢⱼₖₗₘₙₒₚ૧ᵣₛₜᵤᵥwₓᵧ₂₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎"
+	res = x.maketrans(''.join(normal), ''.join(sub_s))
+	return x.translate(res)
+
+# display subscript
+print('H{}SO{}'.format(get_sub('2'),get_sub('4'))) #H₂SO₄
+
